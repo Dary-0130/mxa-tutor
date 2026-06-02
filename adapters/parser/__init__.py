9848af9a2dd1f309e5e03c8a3fadfcca4e88b480
Dary-0130 <ctx6781@gmail.com>
2026-06-03 00:33:27 +0800

    chore(parser): export zip extraction APIs

--- adapters/parser/__init__.py
+++ adapters/parser/__init__.py
@@ -1,4 +1,6 @@
+from adapters.parser.file_classifier import classify_files
 from adapters.parser.m_parser import MParserImpl
 from adapters.parser.slx_parser import SlxParserImpl
+from adapters.parser.zip_extractor import safe_extract
 
-__all__ = ["SlxParserImpl", "MParserImpl"]
+__all__ = ["SlxParserImpl", "MParserImpl", "safe_extract", "classify_files"]
